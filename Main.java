4d330c2170b181e84d04e6c1b424a97d2145aff0
alireza <alireza1212rezaie@gmail.com>
2024-10-25 18:24:15 +0330

    WE CREATE MAIN CLASS AND ADD TO STUDENT AND THEN PRINT INFO

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Student student1 = new Student("James" ," Gosling","0987654",18);
+        Student student2 = new Student("Dennis"," Ritchie","1234556",17.5);
+        student1.printStudentInfo();
+        student2.printStudentInfo();
+
+    }
+}
